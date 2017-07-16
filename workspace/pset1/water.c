a1e7715487202e8f7eb67dcb90f1015dbed9fc6b
Brian Dart <dartb@uw.edu>
2017-07-16 13:04:10 -0700

    Refactored water with a few helper functions.

--- workspace/pset1/water.c
+++ workspace/pset1/water.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
 #include <cs50.h>
 
+int ouncesPerMinutes(int minutes);
+int bottlesPerOunces(int ounces);
+
 int main(void)
 {
     printf("Minutes: ");
     int minutes = get_int();
-    int ounces = minutes * 192;
-    int bottles = ounces / 16;
+    int ounces = ouncesPerMinutes(minutes);
+    int bottles = bottlesPerOunces(ounces);
     printf("Bottles: %i\n", bottles);
+}
+
+int ouncesPerMinutes(int minutes)
+{
+    return minutes * 192;
+}
+
+int bottlesPerOunces(int ounces)
+{
+    return ounces/16;
 }
